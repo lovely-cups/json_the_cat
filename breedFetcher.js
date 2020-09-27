62c997558d1a8a3c4effdfc9c4860665d5a74437
‘Charles <charlesthompson.2693@gmail.com>
2020-09-27 21:58:45 +0000

    refactored for M&C testing

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,9 +1,7 @@
 const request = require('request');
 
-const breed = process.argv[2];
 
-
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, function (error, response, body) {
+request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`,  (error, response, body) => {
 const data = JSON.parse(body);  
 const description = data[0].description;
 if(description === undefined) {
